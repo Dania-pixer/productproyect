a1184694bfc0da8f86fabe1d5cad8590bdfa011b
Dania-pixer <daniasofiaserranoperdomo@gmail.com>
2025-11-20 11:20:19 -0500

    Refactor AWS S3 initialization to use new SDK

--- s3.js
+++ s3.js
@@ -1,10 +1,18 @@
-import AWS from "aws-sdk";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-export const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
+import {
+  S3Client,
+  PutObjectCommand,
+  DeleteObjectCommand
+} from "@aws-sdk/client-s3";
+
+export const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
 });
+
+export { PutObjectCommand, DeleteObjectCommand };
